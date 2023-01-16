182f1cd674573839e143a862c735a6e88da3f4d1
unknown <tsvettyrz12@gmail.com>
2023-01-16 22:14:54 +0200

    Delete comments

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,16 @@
 import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.lenght === 0) {
         return <p> No feedback Yet</p>
     }
     return <div className="'feedback-list">
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item}/>
+            <FeedbackItem key={item.id} item={item}
+            handleDelete= {handleDelete}/>
         ))}
     </div>
 }
+
 
 export default FeedbackList
